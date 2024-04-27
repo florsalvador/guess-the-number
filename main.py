c941446138f05359a9067ad1bbe95ae679a0cbb0
florsalvador <florsalvador219@gmail.com>
2024-04-26 19:49:13 -0500

    Se agrega la opcion de volver a jugar y ahora el bucle está dentro de otro bucle while

--- main.py
+++ main.py
@@ -1,97 +1,57 @@
+""""Module imported to generate random number"""
 import random
 
-numToGuess = random.randint(1, 100)
-print(numToGuess)
+def random_num():
+    """Returns a random number between 1 and 100"""
+    return random.randint(1, 100)
 
-guessesPlayer = []
-guessesComputer = []
+number_guessed = False
 
-def randonNum():
-  return random.randint(1, 100)
+def play(player, guesses, secret_num):
+    """Compares players' guesses to the secret number"""
+    print(f"----{player}----")
+    player_num = (
+        int(input("Player 1: Enter your guess: "))
+        if player == "Player 1"
+        else random_num()
+    )
+    guesses.append(player_num)
+    if player == "Computer":
+        print("Computer: Enter your guess:", player_num)
+    if player_num < secret_num:
+        print("Wrong, too low!\n")
+    elif player_num > secret_num:
+        print("Wrong, too high!\n")
+    else:
+        print(f"Congratulations! {player} guessed the number")
+        print("Attempts:", guesses)
+        print("Total attempts:", len(guesses))
+        global number_guessed
+        number_guessed = True
 
-numberGuessed = False
-
-def play(player, guesses, secretNum):
-  print(f"----{player}----")
-  playerNum = int(input("Player 1: Enter your guess: ")) if player == "Player 1" else randonNum()
-  guesses.append(playerNum)
-  if player == "Computer":
-    print("Computer: Enter your guess:", playerNum)
-  if playerNum < secretNum:
-    print("Wrong, too low!\n")
-  elif playerNum > secretNum:
-    print("Wrong, too high!\n")
-  else:
-    print(f"Congratulations! {player} guessed the number")
-    print("Attempts:", guesses)
-    print("Total attempts:", len(guesses))
-    global numberGuessed 
-    numberGuessed = True
-
-while not numberGuessed:
-  play("Player 1", guessesPlayer, numToGuess) # por que si cambia numberGuessed a True, el bucle no se rompe aqui?
-  if numberGuessed == True:
-    break
-  else:
-    play("Computer", guessesComputer, numToGuess)
-
-
-
-  # print("----Player 1----")
-  # playerNum = int(input("Player 1: Enter your guess: "))
-  # guessesPlayer.append(playerNum)
-  # if playerNum != numToGuess:
-  #   if playerNum < numToGuess:
-  #     print("Too low\n")
-  #   else:
-  #     print("Too high\n")
-  #   print("----Computer----")
-  #   computerNum = random.randint(1, 100)
-  #   guessesComputer.append(computerNum)
-  #   print("Computer: Enter your guess:", computerNum)
-  #   if computerNum != numToGuess:
-  #     if computerNum < numToGuess:
-  #       print("Too low\n")
-  #     else:
-  #       print("Too high\n")
-  #   else:
-  #     print("Congratulations! Computer guessed the number")
-  #     print("Attempts:", guessesComputer)
-  #     print("Total attempts:", len(guessesComputer))
-  #     break
-  # else:
-  #   print("Congratulations! You guessed the number")
-  #   print("Attempts:", guessesPlayer)
-  #   print("Total attempts:", len(guessesPlayer))
-  #   break
-
-# playAgain = input("Do you want to play again? Type Y or N")
-
-# while playerNum != secretNum:
+while True:
+    num_to_guess = random_num()
+    print(num_to_guess)
+    guesses_player = []
+    guesses_computer = []
+    while not number_guessed:
+        play("Player 1", guesses_player, num_to_guess)
+        if number_guessed is True:
+            break
+        play("Computer", guesses_computer, num_to_guess)
+        if number_guessed is True:
+            break
+    playAgain = input("\nDo you want to play again? Type y for yes ")
+    if playAgain == "y":
+        number_guessed = False
+    else:
+        print("Thank you for playing :)")
+        break
 
 
-# for guess in guesses:
-#   if guess != secretNum:
-#     if guess < secretNum:
-#       print("Too low")
-#     else:
-#       print("Too high")
-
-
-# if playerNum != secretNum:
-#   if playerNum < secretNum:
-#     print("Too low")
-#   else:
-#     print("Too high")
-#   print("----Computer----")
-#   computerNum = random.randint(1, 100)
-#   print("Computer: Enter your guess:", computerNum)
-#   if computerNum != secretNum:
-#     if computerNum < secretNum:
-#       print("Too low")
-#     else:
-#       print("Too high")
-#   else:
-#     print("Congratulations! Computer guessed the number")
-# else:
-#   print("Congratulations! You guessed the number")
+# por que si cambia numberGuessed a True, el bucle no se rompe aqui?
+# while not number_guessed:
+#     play("Player 1", guesses_player, num_to_guess)
+#     if number_guessed is True:
+#         break
+#     play("Computer", guesses_computer, num_to_guess)
