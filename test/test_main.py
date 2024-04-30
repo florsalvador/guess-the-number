a4105d37c132cfd5848e71db5176ac12e0923a17
florsalvador <florsalvador219@gmail.com>
2024-04-30 18:41:59 -0500

    Se modifican los parámetros de la función verify en main.py y se agregan pruebas unitarias en test_main.py

--- test/test_main.py
+++ test/test_main.py
@@ -1,16 +1,15 @@
 """"functions imported from main.py"""
-from main import computer_guess
+from main import computer_guess, verify
 
 
 def test_computer_guess():
-    """Testing computer_guess function"""
+    """Testing function computer_guess(last_guess, last_hint)"""
     assert computer_guess(99, "too low") == 100
+    assert computer_guess(2, "too high") == 1
+    assert computer_guess(10, "too high") < 10
 
 
-
-# guesses_player = [0]
-# hints =  ["", ""]
-
-# def test_verify():
-#     """Testing verify function"""
-#     assert verify("Player 1", 10, guesses_player, 20, hints)
+def test_verify():
+    """Testing function verify(guess, secret_num, guesses, hint)"""
+    assert verify(20, 20, [0], ["", ""]) is True
+    assert verify(15, 20, [0], ["", ""]) is False
